b67ed846cdae4daf4fe3ef6115d53bb702c8a06d
Ellen Saylor <ellensaylor8@gmail.com>
2019-08-13 10:25:00 -0500

    completed questions 3&4

--- database.sql
+++ database.sql
@@ -19,8 +19,18 @@ INSERT INTO accounts (username, city, transactions_completed, transactions_attem
 ('hope', 'phoenix', 4, 10, 50.17);
 
 --1. How do you get all users from Chicago?
-SELECT * FROM "accounts" WHERE "city" = 'chicago'
+SELECT * FROM "accounts" WHERE "city" = 'chicago';
 
 --2. How do you get all users with usernames that contain the letter a?
-SELECT * FROM "accounts" WHERE "username" ILIKE '%a%'
+SELECT * FROM "accounts" WHERE "username" ILIKE '%a%';
+
+--3. The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
+UPDATE "accounts" SET "account_balance" = 10 WHERE "account_balance" = 0.00 AND "transactions_attempted" = 0;
+
+--4. How do you select all users that have attempted 9 or more transactions?
+SELECT * FROM "accounts" WHERE "transactions_attempted" >= 9;
+
+--5. How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+
+
 
